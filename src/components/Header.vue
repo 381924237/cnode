380138b245871efbf51cd409de2253e0ccd462a4
XXXXzy <381924237@qq.com>
2019-02-16 22:28:40 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="header">
     <div class="container">
-      <a class="logo" href="/">
+      <a class="logo" href="">
         <img src="../assets/cnodejs_light.svg" alt="">
       </a>
       <ul>
-        <li><a href="/">首页</a></li>
+        <li><a href="">首页</a></li>
         <li><a href="#">新手入门</a></li>
         <li><a href="#">API</a></li>
         <li><a href="#">关于</a></li>
